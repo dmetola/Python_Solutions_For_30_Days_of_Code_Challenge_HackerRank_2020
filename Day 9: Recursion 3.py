900ff9015a008747ce629ef389c152ff3ba94f72
Dario Metola Rodriguez <65161098+dmetola@users.noreply.github.com>
2020-06-16 12:21:07 +0100

    Create Day 9: Recursion 3.py

---
+++ Day 9: Recursion 3.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def factorial(num):
+    return 1 if num == 1 else factorial(num - 1) * num
+
+
+print(factorial(int(input())))
+
+
+print(factorial(n-1))
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+    
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
